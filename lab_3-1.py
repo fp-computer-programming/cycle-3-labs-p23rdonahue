49244f0c53a5058f6c9cebec3e1fb96829c96b6a
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-09-27 10:44:15 -0400

    lab 3-1 complete

---
+++ lab_3-1.py
@@ -0,0 +1,7 @@
+# author: RED 09/27/22
+magic_charge= 8
+shield_charge= 99
+if (magic_charge >= 90 and shield_charge >= 75):
+    print("You defeated the dragon! But the pricess is in another casle.")
+else:
+    print("The dragon burns ya boy")
