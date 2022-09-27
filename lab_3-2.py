05780d4ca2648915933a95efb4a9c508a80cf1a2
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-09-27 10:32:24 -0400

    lab 3-2 complete

---
+++ lab_3-2.py
@@ -0,0 +1,12 @@
+#author:RED 09/27/22
+
+#int(a)
+#print(a) - name error
+#this is a name error
+#print('a') - value error
+# this is a value error
+#'a' + 2 - type error
+# type error
+#import date - module not found
+#module not found
+print("I'm a happy camper!)
