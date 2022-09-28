4f202a35b4cbb8aff879f2ffcac772f9f7f9a2cd
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-09-28 10:49:03 -0400

    lab 3-3 complete

---
+++ lab_3-3.py
@@ -0,0 +1,14 @@
+#author: RED 09/28/22
+import random
+x= random. randint(1,100)
+
+random. randint(1,100)
+print(x)
+#I got 94
+z= random. seed(58)
+print(random.random())
+#0.5807571649339567
+
+b=random.randrange(0,100,2)
+print(b)
+#26
