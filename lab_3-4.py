ce34ad11e0ca7b71cbf7e2ee79e009239eace455
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-09-29 12:05:01 -0400

    lab 3-4 completed

---
+++ lab_3-4.py
@@ -0,0 +1,32 @@
+# author: RED 09/29/22
+import math
+#8
+#19
+#42
+#8
+#2
+#3
+#1
+#120
+#1
+#0
+a= math.ceil(7.15)
+b=math.floor(19.156)
+c=math.fabs(-42)
+d=math.pow(2,3)
+e=math.log10(100)
+f=math.sqrt(9)
+g=math.isqrt(2)
+h=math.factorial(5)
+i=math.gcd(1111111111, 12321)
+j=math.sin(0)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
