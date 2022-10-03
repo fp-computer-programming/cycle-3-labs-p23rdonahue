2aa60f6324dc08ce88940a3841175eab8105263c
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-10-03 14:28:04 -0400

    lab_3-6 complete

---
+++ lab_3-6.py
@@ -0,0 +1,17 @@
+# author: RED 10/3/21
+
+var_1 = 'Fairfield'
+var_2 = 'Prep'
+print (var_1 + ' ' + var_2)
+
+var_3 = '-'
+print(var_3 * 20)
+
+var_4= 'I\'m loving this short story I\'m reading, \"The Fall of the House of Usher\"'
+print (var_4)
+
+find_length = len('I\'m loving this short story I\'m reading, \"The Fall of the House of Usher\"')
+print(find_length)
+
+fruit = 'apple'
+print(fruit[3])
